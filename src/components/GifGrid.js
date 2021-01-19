4ac021372587eeb8392630492291beaafd8b184b
stevenacz <scoailazaa@gmail.com>
2021-01-19 18:18:44 -0500

    lalal

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -6,20 +6,22 @@ const GifGrid = ( { category } ) => {
   const { data:images, loading } = useFetchGifs( category );
 
   return (
-    <div>
+    <>
       <h3>{ category }</h3>
 
       { loading && <p>Cargando...</p> }
 
-      {
-        images.map( img => (
-          <GifGridItem
-            key={ img.id }
-            { ...img }
-          />
-        ))
-      }
-    </div>
+      <div className="card-grid">
+        {
+          images.map( img => (
+            <GifGridItem
+              key={ img.id }
+              { ...img }
+            />
+          ))
+        }
+      </div>
+    </>
   )
 };
 
--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GifGridItem = ( { url, title } ) => {
   return (
-    <div>
+    <div className="card">
       <img src={ url } alt={ title } />
       <p>{ title }</p>
     </div>
